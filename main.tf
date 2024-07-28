1a6aeb9d316239eb830a2b9bc20a331e6e4b2efe
sss-kato <syunpei.1124@gmail.com>
2024-07-28 23:36:35 +0900

    RDS パラメーター

--- main.tf
+++ main.tf
@@ -191,12 +191,31 @@ resource "aws_security_group" "db_sg" {
 }
 
 resource "aws_security_group_rule" "db_inbound" {
-  security_group_id        = aws_security_group.web_sg.id
+  security_group_id        = aws_security_group.db_sg.id
   type                     = "ingress"
   protocol                 = "tcp"
   from_port                = "3306"
   to_port                  = "3306"
   source_security_group_id = aws_security_group.web_sg.id
+}
+
+
+# RDS
+resource "aws_db_parameter_group" "mysql_parametergroup"{
+        name = "${var.project}-${var.env}-mysql"
+        family = "mysql8.0"
+
+        parameter{
+                name = "character_set_database"
+                value = "utf8mb4"
+        }
+
+        parameter{
+                name = "character_set_server"
+                value = "utf8mb4"
+        }
+
+
 }
 
 
