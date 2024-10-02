865bad0705ea5154105735a463f8d705501a7a66
sss-kato <syunpei.1124@gmail.com>
2024-10-02 21:42:13 +0900

    RDS構築

--- main.tf
+++ main.tf
@@ -240,28 +240,44 @@ resource "aws_db_subnet_group" "mysql_subnet_group" {
   }
 }
 
-resource "aws_db_instance" "mysql_instance"{
-        engine = "mysql"
-        engine_version = "8.0.20"
+resource "aws_db_instance" "mysql_instance" {
+  engine         = "mysql"
+  engine_version = "8.0.39"
 
-        identifier = "${var.project}-${var.env}-mysql-instance"
-        
-        username = "admin"
-        password = ""
+  identifier = "${var.project}-${var.env}-mysql-instance"
 
-        instance_class = "db.t2.micro"
+  username = "admin"
+  password = "admin202410"
 
-        allocated_storage = 20
-        max_allocated_storage = 50
-        storage_type = "gb2"
-        storage_encrypted = false
+  instance_class = "db.t3.micro"
 
-        multi_az = false
-        availability_zone = "ap-northeast-1a"
-        db_subnet_group_name = aws_db_subnet_group.mysql_subnet_group.name
-        vpc_security_group_ids = [aws_security_group.db_sg.id]
-        public_accessible = false
-        port = 3306
+  allocated_storage     = 20
+  max_allocated_storage = 50
+  storage_type          = "gp2"
+  storage_encrypted     = false
+
+  multi_az               = false
+  availability_zone      = "ap-northeast-1a"
+  db_subnet_group_name   = aws_db_subnet_group.mysql_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  publicly_accessible      = false
+  port                   = 3306
+
+  backup_window             = "04:00-05:00"
+  backup_retention_period    = 7
+  maintenance_window         = "Mon:05:30-Mon:08:30"
+  auto_minor_version_upgrade = false
+
+  deletion_protection = true
+  skip_final_snapshot = true
+
+  apply_immediately = true
+
+  tags = {
+    Name    = "${var.project}-${var.env}-mysql-instance"
+    Project = var.project
+    Env     = var.env
+  }
 
 
 
