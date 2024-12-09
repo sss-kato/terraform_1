5476076f63a36ba66d869feff1bd95a2c374d0e9
sss-kato <syunpei.1124@gmail.com>
2024-12-10 06:52:43 +0900

    AMI取得

--- main.tf
+++ main.tf
@@ -297,6 +297,25 @@ resource "aws_lb" "alb" {
   ]
 }
 
+resource "aws_lb_target_group" "alb_target_group" {
+  name     = "${var.project}-${var.env}-app-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
+  tags = {
+    Name    = "${var.project}-${var.env}-app-tg"
+    Project = var.project
+    Env     = var.env
+  }
+}
+
+# resource "aws_lb_target_group_attachment" "ec2"{
+#         target_group_arn = aws_lb_target_group.alb_target_group.arn
+#         target_id = aws_instance.
+# }
+
+
+
 
 
 
